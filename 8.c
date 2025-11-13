fe55f752785bdd651f9558e52ee329aea2b1fee8
shubham-nov06 <dograshubham06@gmail.com>
2025-11-13 13:48:14 +0530

    Added file 8.c

---
+++ 8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int ;
+    int   ;
+    int arr[99] = {10, 20, 30, 40, 50};
+    printf("Enetr the place you want to insert at = ");
+    scanf(k, &k);
+    printf("Enter the number you want to enter");
+    scanf(value,&value);
+    for(int i = k ; i > value ; i-- ) {
+        arr[i] =  arr[i - 1];
+
+    }
+arr[value] =VID1
+
+}
