c0588dbc90bfbe4de90a0bce5389225cc0e45dc7
shubham-nov06 <dograshubham06@gmail.com>
2025-11-13 13:48:13 +0530

    Added file 5.c

---
+++ 5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main( ) {
+int *a ;
+int arr[10] = { 10 ,20 ,30 ,40 ,50 };
+a = arr ;
+printf("%d \n" , a );
+printf("%d\n" ,&a) ;
+printf("%d \n " , *a) ;
+printf("%d\n" , a[0]);
+printf("%d\n" , (a+1) );
+printf("%d\n" ,&a[1]) ;
+printf("%d \n " , *(a + 1)) ;
+printf("%d\n" , a[1]);
+return 0 ;
+
+
+
+}
